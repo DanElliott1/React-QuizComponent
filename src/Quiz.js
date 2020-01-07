846ee07d2b78f04e01733907a8b41d0a8ccf86af
Dan Elliott <Dan.Elliott@coxautoinc.com>
2020-01-07 10:57:21 -0500

    module4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
-import QuizQuestion from './QuizQuestion'
+import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 
@@ -14,8 +15,14 @@ class Quiz extends Component {
     }
 
     render() {
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
-            <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /></div>
+            <div>
+                { 
+                    isQuizEnd ? <QuizEnd /> :
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                }
+            </div>
         )
     }
 }
